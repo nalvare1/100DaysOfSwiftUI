0bd2ddcca78183a2f4762e3b47401bf74525dbce
nalvare1 <nalvare1@nd.edu>
2023-12-06 17:26:02 -0800

    Day 22

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -11,8 +11,19 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
 
-    @State private var showingScore = false
+    @State private var scoreRight = 0
+    @State private var scoreWrong = 0
     @State private var scoreTitle = ""
+    @State private var scoreIsShowing = false
+    @State private var gameOver = false
+
+    var score: String {
+        "\(scoreRight) right\n\(scoreWrong) wrong"
+    }
+
+    var gamesPlayed: Int {
+        scoreRight + scoreWrong
+    }
 
     var body: some View {
         ZStack {
@@ -58,7 +69,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
 
-                Text("Score: ???")
+                Text("Score:\n\(score)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
 
@@ -68,22 +79,48 @@ struct ContentView: View {
 
 
         }
-        .alert(scoreTitle, isPresented: $showingScore) {
+        .alert(scoreTitle, isPresented: $scoreIsShowing) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is\n\(score)")
+        }
+        .alert(scoreTitle, isPresented: $gameOver) {
+            Button("Play Again", action: restartGame)
+        } message: {
+            Text("Your final score is\n\(score)")
         }
     }
 
     func flagTapped(_ option: Int) {
-        scoreTitle = (option == correctAnswer) ? "Correct" : "Wrong"
+        if (option == correctAnswer) {
+            scoreRight += 1
+            scoreTitle = "Correct"
+        } else {
+            scoreWrong += 1
+            scoreTitle = "Wrong! That's the flag of \(countries[option])"
+        }
 
-        showingScore = true
+        scoreIsShowing = true
+
+        if (gamesPlayed >= 3) {
+            gameOver = true
+            scoreIsShowing = false
+        } else {
+            scoreIsShowing = true
+        }
     }
 
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+
+    func restartGame() {
+        scoreRight = 0
+        scoreWrong = 0
+
+        gameOver = false
+        askQuestion()
     }
 }
 
