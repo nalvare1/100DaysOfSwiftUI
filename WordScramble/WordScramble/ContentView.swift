ca4701d576ae7461f6bbb90cc4b57d153254c742
nalvare1 <nalvare1@nd.edu>
2024-02-26 15:07:08 -0800

    WordScramble

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -12,13 +12,19 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
 
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+
+    @State private var score = 0
+
     var body: some View {
         NavigationStack {
             List {
                 // Add new word:
                 Section {
                     TextField("Enter your word", text: $newWord)
-                        //.textInputAutocapitalization(.never)
+                        .textInputAutocapitalization(.never)
                 }
 
                 // List of used words:
@@ -30,27 +36,67 @@ struct ContentView: View {
                         }
                     }
                 }
+
+                // Score - Day 31:
+                Section(header: Text("Score")) {
+                    Text("\(score)")
+                }
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) { } message: {
+                Text(errorMessage)
+            }
+            // Day 31:
+            .toolbar {
+                Button("New Game", action: startGame)
+            }
         }
     }
 
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
 
-        // if answer is empty, return:
-        guard answer.count > 0 else { return }
+        guard isNotEmpty(word: answer) else { return }
+
+        guard isNotTooShort(word: answer) else {
+            wordError(title: "Word is too short", message: "Your word must have at least 3 letters!")
+            return
+        }
+
+        guard isNotRootWord(word: answer) else {
+            wordError(title: "Word is root word", message: "You can't just use the question as the answer!")
+            return
+        }
+
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
 
         // add answer to the top of the list of usedWords:
         withAnimation {
             usedWords.insert(answer, at: 0)
+            calculateScore(word: answer)
         }
         newWord = ""
     }
 
     func startGame() {
+        score = 0
+        usedWords = []
+
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allWords = startWords.components(separatedBy: "\n")
@@ -62,8 +108,55 @@ struct ContentView: View {
         fatalError("Could not load start.txt from bundle.")
     }
 
+    // Original Solution - Day 30:
     func isOriginal(word: String) -> Bool {
         !usedWords.contains(word)
+    }
+
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                // letter is not found in word:
+                return false
+            }
+        }
+
+        return true
+    }
+
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+
+    // Challenge Solution - Day 31:
+    func isNotEmpty(word: String) -> Bool {
+        return word.count != 0
+    }
+
+    func isNotTooShort(word: String) -> Bool {
+        return word.count >= 3
+    }
+
+    func isNotRootWord(word: String) -> Bool {
+        return word != rootWord
+    }
+
+    func calculateScore(word: String) {
+        score += word.count
     }
 }
 
